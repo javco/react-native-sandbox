5534ab61a0a73f1f4062629b7fe7be34d14bccb2
javco <jvrcastilla@gmail.com>
2018-04-20 15:21:11 +0200

    some flexbox

--- app/components/HelloWorld/HelloWorld.js
+++ app/components/HelloWorld/HelloWorld.js
@@ -34,6 +34,23 @@ export default class HelloWorld extends Component {
         <Text style={styles.myText}>
           {fullName}
         </Text>
+        <View style={styles.containerv1}>
+          <View style={styles.box1}>
+            <Text style={styles.myText}>
+              Box 1
+            </Text>
+          </View>
+          <View style={styles.box2}>
+            <Text style={styles.myText}>
+              Box 2
+            </Text>
+          </View>
+          <View style={styles.box3}>
+            <Text style={styles.myText}>
+              Box 3
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -47,6 +64,27 @@ const styles = StyleSheet.create({
   },
   myText: {
     color: 'white'
+  },
+  containerv1: {
+    flexDirection: 'row',
+    height: 100,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  box1: {
+    flex: 1,
+    backgroundColor: 'tomato',
+    padding: 10,
+  },
+  box2: {
+    flex: 1,
+    backgroundColor: '#4286f4',
+    padding: 10,
+  },
+  box3: {
+    flex: 1,
+    backgroundColor: '#1dd18c',
+    padding: 10,
   }
 })
 
